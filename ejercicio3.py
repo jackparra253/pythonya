80702101bfd6faae2eb9c2e4d1449b2bd2b83d0e
Jack <jack.parra253@gmail.com>
2019-01-07 17:35:38 -0500

    ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,12 @@
+# Realizar la carga de dos numeros enteros por teclado e imprimir su suma y su producto.
+
+numero_uno = input("Ingrese el primer numero ")
+numero_uno = int(numero_uno)
+numero_dos = input("Ingrese el segundo numero ")
+numero_dos = int(numero_dos)
+
+suma = numero_uno + numero_dos
+producto = numero_uno * numero_dos
+
+print "La suma de los dos numero es: " + str(suma)
+print "El producto de los dos numero es: " + str(producto)
