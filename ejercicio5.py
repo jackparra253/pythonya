858c1f455abf5c4ff9c2219202032818bb90c262
Jack <jack.parra253@gmail.com>
2019-01-07 18:03:35 -0500

    ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+# Realizar la carga del lado de un cuadrado, mostrar por pantalla el perimetro 
+# del mismo (El perimetro de un cuadrado se calcula multiplicando el valor 
+# del lado por cuatro)
+
+lado = input("Ingrese el lado del cuadrado ")
+lado = int(lado)
+perimetro = lado * 4
+
+print "El perimetro del cuadrado es: " + str(perimetro)
