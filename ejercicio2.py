298d5b3ab6fa3f2051212414ed59bfddeb46e4d5
Jack <jack.parra253@gmail.com>
2019-01-07 13:56:20 -0500

    ok

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+# Hallar la superficie de un cuadrado conociendo el valor de un lado.
+
+lado = input("Ingrese el lado de un cuadrado ")
+lado = int(lado)
+superficie = lado * lado
+print("La superficie del cuadrado es:")
+print(superficie)
+
