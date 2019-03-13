b8f01e23043ff1c38c5c3c0f4239129903e41074
Jack <jack.parra253@gmail.com>
2019-03-12 23:11:22 -0500

    Ejercicio 11

---
+++ ejercicio011.py
@@ -0,0 +1,14 @@
+num1 = int(input("Ingrese primer valor."))
+num2 = int(input("Ingrese segundo valor."))
+
+if num1 > num2:
+    suma = num1 + num2
+    resta = num1 - num2
+    print(suma)
+    print(resta)
+else:
+    producto = num1*num2
+    division = num1/num2
+    print(producto)
+    print(division)
+
